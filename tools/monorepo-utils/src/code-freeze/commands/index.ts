c491d458f2231899c05762bd824b772e59f49cb3
Paul Sealock <psealock@gmail.com>
2023-04-14 06:59:46 +1200

    reorganize folders

--- tools/monorepo-utils/src/code-freeze/commands/index.ts
+++ tools/monorepo-utils/src/code-freeze/commands/index.ts
@@ -7,7 +7,7 @@ import { Command } from '@commander-js/extra-typings';
  * Internal dependencies
  */
 import { verifyDayCommand } from './verify-day';
-import { milesStoneCommand } from './milestone/milestone';
+import { milesStoneCommand } from './milestone';
 
 const program = new Command( 'code-freeze' )
 	.description( 'Code freeze utilities' )
